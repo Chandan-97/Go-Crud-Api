2c06f0f761400554269b8dadac2526d3dfa5d1e5
Chandan <chandan@lidolearning.com>
2019-09-22 07:45:14 +0530

    02 Running a server

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,17 @@
 package main
 
-import(
-    "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func main(){
-    fmt.Println("Hello World")
+func main() {
+	e := echo.New()
+	e.GET("/", get)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func get(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from Server")
 }
