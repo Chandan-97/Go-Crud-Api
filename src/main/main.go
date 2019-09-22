5d24da0156b777f130f104e1d5714758da6a3614
Chandan <chandan@lidolearning.com>
2019-09-22 10:45:32 +0530

    03 Adding Routes

--- src/main/main.go
+++ src/main/main.go
@@ -8,10 +8,36 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", get)
+
+	e.GET("/candidates", get)
+	e.GET("/candidates/:id", getCandidate)
+	e.POST("/candidates", post)
+	e.PUT("/candidates/:id", update)
+	e.DELETE("/candidates/:id", delete)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+// func get(c echo.Context) error {
+// 	return c.String(http.StatusOK, "Hello from Server")
+// }
+
 func get(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello from Server")
+	return c.String(http.StatusOK, "GET /candidates")
+}
+
+func getCandidate(c echo.Context) error {
+	return c.String(http.StatusOK, "GET /candidates/:id")
+}
+
+func post(c echo.Context) error {
+	return c.String(http.StatusOK, "POST /candidates/:id")
+}
+
+func update(c echo.Context) error {
+	return c.String(http.StatusOK, "PUT /candidates/:id")
+}
+
+func delete(c echo.Context) error {
+	return c.String(http.StatusOK, "DELETE /candidates/:id")
 }
